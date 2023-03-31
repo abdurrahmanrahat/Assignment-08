5f4668ff9cc91010e7b3b7a3e4e1860477b03bc8
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-03-31 15:09:48 +0600

    blog card add

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Blog.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+
+const Blog = (props) => {
+
+    const { id, coverImage, authorImage, name, title, read } = props.blog;
+
+    return (
+        <div className='blog'>
+            <img className='blog-img' src={coverImage} alt="" />
+            <div className="author-info">
+                <div className='author-img-name'>
+                    <img src={authorImage} alt="" />
+                    <div className='author-name-date'>
+                        <h4>{name}</h4>
+                        <p>Mar 24 (6 Days ago)</p>
+                    </div>
+                </div>
+
+                <p>{read} <span className='bookmark-icon'><FontAwesomeIcon icon={faBookmark} /></span></p>
+            </div>
+            <h2>{title}</h2>
+            <a href="#">Mark as read</a>
+        </div>
+    );
+};
+
+export default Blog;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css'
+import Blog from '../Blog/Blog';
 
 const Main = () => {
+    const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
         fetch('blog.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setBlogs(data))
     }, [])
 
     return (
         <main className='main-body'>
             <div className="blog-container">
-
+                {
+                    blogs.map(blog => <Blog blog={blog}></Blog>)
+                }
             </div>
             <div className="sitebar-container">
                 <h2>sitebar</h2>
